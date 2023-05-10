96211a193fe3cb498454404614ab4bef4ff0645b
lukebonner <lukebonner@lbon.co.uk>
2023-05-10 11:04:35 +0000

    Delete 'scripts/_common.sh'

--- scripts/_common.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#=================================================
-# SET ALL CONSTANTS
-#=================================================
-
-# dependencies used by the app
-#REMOVEME? pkg_dependencies="postgresql"
